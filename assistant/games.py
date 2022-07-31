d329dd8906c824c454bb132cc1de3239d89370d8
Malitha Mihiranga <109048518+xMalitha@users.noreply.github.com>
2022-07-31 09:14:30 +0530

    Update games.py

--- assistant/games.py
+++ assistant/games.py
@@ -5,7 +5,7 @@
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
-"""
+"""kkk
 • `{i}akinator` | `/akinator`
    Start akinator game from Userbot/Assistant
 
